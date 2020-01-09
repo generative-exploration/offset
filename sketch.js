9448b098b6eb0b911651a96c80151bd216bbba2d
generative-exploration <59022519+generative-exploration@users.noreply.github.com>
2020-01-09 11:51:23 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,13 +7,11 @@ let myFont;
 
 function preload() {
   img = loadImage("0010.jpg");
-  myFont = loadFont('FiraCode-Regular.ttf');
 }
 
 function setup() {
   let c = createCanvas(960, 1200);
   image(img, 0, 0);
-  textFont(myFont);
   textSize(14);
 }
 
